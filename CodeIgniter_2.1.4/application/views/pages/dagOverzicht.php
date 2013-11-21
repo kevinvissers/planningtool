492613f5a32cc8619105be2c1ad0bca539ddbde5
DylanDerwael <1135195@student.khlim.be>
2013-11-21 11:01:09 +0100

    Update dagOverzicht.php
    basisi inhoud voor dagOverzicht view

--- CodeIgniter_2.1.4/application/views/pages/dagOverzicht.php
+++ CodeIgniter_2.1.4/application/views/pages/dagOverzicht.php
@@ -1,7 +1,11 @@
-<?php
+<!-- Main Page Content and Sidebar -->
+<div class="row">
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+<!-- Main Blog Content -->
+<div class="large-9 columns" role="content">
+    <h3>dag overzicht</h3>
+    <div class="kalender_dag_inhoud" align="center"><?php echo $kalender; ?></div>
+</div>
+<!-- End Main Content -->
+</div>
+<!-- End Main Content and Sidebar -->
