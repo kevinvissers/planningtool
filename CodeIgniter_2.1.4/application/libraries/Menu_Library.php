5a6cfaf0225b0968aa87e98cbc9ee69e82e26593
Kevin Vissers <kevin.vissers@telenet.be>
2013-11-24 14:14:24 +0100

    extra menu-items

--- CodeIgniter_2.1.4/application/libraries/Menu_Library.php
+++ CodeIgniter_2.1.4/application/libraries/Menu_Library.php
@@ -18,7 +18,10 @@ class Menu_Library {
 /**
  * @access	private 
  */
-    private $activeMenu = '';
+    private $currentController = '';
+    private $loggedIn = false;
+    private $user = '';
+    private $userRole = 0;
     /********************* METHODS ********************/
 /**
  * @access	public
@@ -26,7 +29,10 @@ class Menu_Library {
     public function __construct($params)
     {
         // Do something with $params
-        $this->activeMenu = $params['active'];
+        $this->currentController = $params['currentController'];
+        $this->loggedIn = $params['loggedIn'];
+        $this->user = $params['user'];
+        $this->userRole = $params['userRole'];
     }
     /**
      * @author 		Kevin Vissers
@@ -36,7 +42,7 @@ class Menu_Library {
      */
     public function ToonMenu()
     {
-        return '<!-- Nav Bar -->
+        $strHtml = '<!-- Nav Bar -->
 
 		<div class="row">
 		<div class="large-12 columns">
@@ -46,9 +52,7 @@ class Menu_Library {
                         <!-- Title Area -->
                         <li class="name">
                             <h1>
-                                <a href="'.base_url().'index.php">
-                                        Planningtool
-                                </a>
+                                <a href="'.site_url().'">Planningtool</a>
                             </h1>
                         </li>
                         <li class="toggle-topbar menu-icon"><a href="#"><span>menu</span></a></li>
@@ -59,19 +63,35 @@ class Menu_Library {
                               <a class="active" href="#">Kalenderweergaven</a>
                               <ul class="dropdown">
                                 <li><label>Kalenders</label></li>
-                                <li><a href="'.base_url().'index.php/kalender/maandOverzicht">Maandoverzicht</a></li>
-                                <li><a href="'.base_url().'index.php/kalender/weekOverzicht">Weekoverzicht</a></li>
-                                <li><a href="'.base_url().'index.php/kalender/dagOverzicht">dagoverzicht</a></li>
+                                <li><a href="'.site_url().'/kalender/maandOverzicht">Maandoverzicht</a></li>
+                                <li><a href="'.site_url().'/kalender/weekOverzicht">Weekoverzicht</a></li>
+                                <!--<li><a href="'.site_url().'/kalender/dagOverzicht">dagoverzicht</a></li>-->
                               </ul>
                             </li>
                             <!--<li><a href="">Extra</a></li>-->
                         </ul>
-                        <ul class="right">
-                            <li class="has-button">
-                              <a class="small button knopje" href="'.base_url().'index.php/aanmelden/login">Aanmelden</a>
+                        <ul class="left">
+                            <li class="has-dropdown">
+                              <a class="active" href="#">Gebruikers</a>
+                              <ul class="dropdown">
+                                <li><a href="'.site_url().'/gebruiker/toevoegen">Toevoegen</a></li>
+                                <li><a href="'.site_url().'/gebruiker/bewerken">Bewerken</a></li>
+                                <li><a href="'.site_url().'/gebruiker/verwijderen">Verwijderen</a></li>
+                              </ul>
                             </li>
-                        </ul>
-                    </section>
+                            <!--<li><a href="">Extra</a></li>-->
+                        </ul>';
+        if($this->loggedIn){             
+        $strHtml .= '<ul class="right">
+                        <li class="name">
+                            <a href="#">'.$this->user.'</a>
+                        </li>
+                        <li class="has-button">
+                            <a class="small button knopje" href="'.site_url().'/'.$this->currentController.'/logout">Afmelden</a>
+                        </li>
+                    </ul>';
+        }
+        $strHtml .= '</section>
                 </nav>
 		  <h1>Planningtool <small>Here you can plan everything!</small></h1>
 		  <hr />
@@ -79,6 +99,7 @@ class Menu_Library {
 		</div>
 
 	<!-- End Nav -->';
+        return $strHtml;
     }
 }
 
