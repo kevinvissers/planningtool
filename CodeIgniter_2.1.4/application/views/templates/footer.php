24a66d6eb72849ad7a17561ac6781af7f81f8c5b
Kevin Vissers <kevin.vissers@telenet.be>
2013-11-28 08:58:49 +0100

    footer

--- CodeIgniter_2.1.4/application/views/templates/footer.php
+++ CodeIgniter_2.1.4/application/views/templates/footer.php
@@ -5,7 +5,7 @@
 	  <hr />
 	  <div class="row">
             <div class="large-12 columns white">
-              <p><?php echo $device ?><span class="right author size-12">&copy; Designed by &#946;art & Kev!n</span></p>
+              <p><?php echo $device ?><!--<span class="right author size-12">&copy; Designed by &#946;art & Kev!n</span>--></p>
             </div>
 	  </div>
 	</div>
