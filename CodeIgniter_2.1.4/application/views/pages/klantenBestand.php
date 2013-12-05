0171a8a70fcf0f51030622bc1385eed43e815a4c
Bart Bollen <bartbollen@telenet.be>
2013-12-05 09:20:11 +0100

    view klantenbestand

---
+++ CodeIgniter_2.1.4/application/views/pages/klantenBestand.php
@@ -0,0 +1,13 @@
+<!-- Main Page Content and Sidebar -->
+
+	<div class="row">
+
+            <!-- Main Content -->
+            <div class="large-12 columns" role="content">
+                <div><?php echo $inhoud; ?></div> 
+            </div>
+            <!-- End Main Content -->
+	</div>
+
+	<!-- End Main Content and Sidebar -->
+
