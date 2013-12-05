f17ccfd79963f65d7b3c4f1e037cbfd69226dc44
Bart Bollen <bartbollen@telenet.be>
2013-12-05 08:54:35 +0100

    Klanten afgewerkt

--- CodeIgniter_2.1.4/application/controllers/klanten.php
+++ CodeIgniter_2.1.4/application/controllers/klanten.php
@@ -84,6 +84,118 @@ class Klanten extends CI_Controller {
         
         
     }
+    
+    public function Bewerken()
+    {
+        if ( ! file_exists('application/views/pages/afspraakFormulier.php'))
+	{
+		// Whoops, we don't have a page for that!
+		show_404();
+	}
+        $this->load->library('Helper_Library');
+        $data = $this->helper_library->Init();
+        $blnPermission = $this->session->userdata('logged_in') ? true : false;
+        $session_data = $this->session->userdata('logged_in');
+        
+        $menuConfig = array(
+            'currentController' => 'gebruiker',
+            'loggedIn' => $blnPermission,
+            'user' => $session_data['username'],
+            'userRole' => 3
+        );
+        
+        $this->load->library('Menu_Library', $menuConfig);
+        $this->load->model('Klanten_model');
+        
+        if($blnPermission){
+            $data['menu'] = $this->menu_library->ToonMenu();
+            $data['title'] = 'Klant bewerken';
+            
+            if(isset($_POST['Bewerken'])){
+                $data['klantenTabel'] = $this->Klanten_model->KlantTonen($_POST['klant_id']);
+            }
+            if(isset($_POST['updateKlant'])){
+                $arrGegevens = array(
+                    'klantID' => $_POST['klantID'],
+                    'voornaam' => ucfirst($_POST['txtKlantVoornaam']),
+                    'achternaam' => ucfirst($_POST['txtKlantAchternaam']),
+                    'straat' => ucfirst($_POST['txtStraat']),
+                    'huisnummer' => $_POST['txtHuisnummer'],
+                    'idgemeente' => '',
+                    'telefoon' => $_POST['txtTelefoon'],
+                    'gsm' => $_POST['txtGsm'],
+                    'email' => $_POST['txtEmail'],
+                    'opmerking' => $_POST['txtOpmerking']
+                );
+
+                $arrGemeente = array(
+                    'gemeente' => ucfirst($_POST['txtGemeente']),
+                    'postcode' => $_POST['txtPostcode']
+                );
+
+                $resultaat = $this->Klanten_model->Bewerken($arrGegevens, $arrGemeente);
+                $data['klantenTabel'] = $resultaat;
+            }
+                        
+            $data['afspraakFormulier'] = $this->Klanten_model->SelectNaamForm();
+            
+            if(isset($_POST['searchKlant']))
+            {
+                $data['afspraakFormulier'] .= $this->Klanten_model->KlantenFilterTonen($_POST['txtSelectNaam'], $_POST['ddSelectItem']);
+            }
+                                    
+            $this->load->view('templates/header', $data);
+            $this->load->view('pages/afspraakFormulier', $data);
+            $data['device'] = $this->helper_library->CreateFooter();
+            $this->load->view('templates/footer', $data);
+        }
+        else
+        {
+            //redirect('login', 'refresh');
+            header('Location: '.site_url().'/login');
+        }
+    }
+    
+    public function Tonen()
+    {
+        if ( ! file_exists('application/views/pages/afspraakFormulier.php'))
+	{
+		// Whoops, we don't have a page for that!
+		show_404();
+	}
+        $this->load->library('Helper_Library');
+        $data = $this->helper_library->Init();
+        $blnPermission = $this->session->userdata('logged_in') ? true : false;
+        $session_data = $this->session->userdata('logged_in');
+        
+        $menuConfig = array(
+            'currentController' => 'gebruiker',
+            'loggedIn' => $blnPermission,
+            'user' => $session_data['username'],
+            'userRole' => 3
+        );
+        
+        $this->load->library('Menu_Library', $menuConfig);
+        $this->load->model('Klanten_model');
+        
+        if($blnPermission){
+            $data['menu'] = $this->menu_library->ToonMenu();
+            $data['title'] = 'Klant bewerken';
+                                    
+            $data['inhoud'] = $this->Klanten_model->AlleKlantenTonen();
+                                                
+            $this->load->view('templates/header', $data);
+            $this->load->view('pages/klantenBestand', $data);
+            $data['device'] = $this->helper_library->CreateFooter();
+            $this->load->view('templates/footer', $data);
+        }
+        else
+        {
+            //redirect('login', 'refresh');
+            header('Location: '.site_url().'/login');
+        }
+    }
+
     public function logout()
     {
         $this->session->unset_userdata('logged_in');
--- CodeIgniter_2.1.4/application/models/klanten_model.php
+++ CodeIgniter_2.1.4/application/models/klanten_model.php
@@ -161,8 +161,10 @@ class Klanten_model extends CI_Model {
     {        
         try 
         {
+            //Controleren of gemeente al bestaat, zoniet, de gemeente toe te voegen
             $objResult = $this->db->get_where('gemeente',array('gemeente' => $arrGemeente['gemeente']));
-                        
+            
+            //Als de gemeente bestaat, bestaat ook de array uit meerdere regels
             if($objResult->num_rows() >0)
             {
                 foreach ($objResult->result() as $row)
@@ -171,6 +173,8 @@ class Klanten_model extends CI_Model {
                 }
                 $this->db->insert('klanten',$arrGegevens);
             }
+            
+            //Gemeente bestaat niet en wordt toegevoegd
             else
             {
                 $this->db->insert('gemeente',$arrGemeente);
@@ -194,6 +198,289 @@ class Klanten_model extends CI_Model {
         {
             return $ex->getMessage();
         }
+    }
+    
+    public function Bewerken($arrKlantGegevens, $arrGemeente){
+        
+        //Controleren of gemeente al bestaat, zoniet, de gemeente toe te voegen
+            $objResult = $this->db->get_where('gemeente',array('gemeente' => $arrGemeente['gemeente']));
+            
+            //Als de gemeente bestaat, bestaat ook de array uit meerdere regels
+            if($objResult->num_rows() >0)
+            {
+                foreach ($objResult->result() as $row)
+                {
+                    $arrKlantGegevens['idgemeente'] = $row->Idgemeente;
+                }
+                $this->db->where('klantID', $arrKlantGegevens['klantID']);
+                $this->db->update('klanten',$arrKlantGegevens);
+            }
+            
+            //Gemeente bestaat niet en wordt toegevoegd
+            else
+            {
+                $this->db->insert('gemeente',$arrGemeente);
+                
+                $objResult2 = $this->db->get_where('gemeente',array('gemeente' => $arrGemeente['gemeente']));
+                foreach ($objResult2->result() as $row)
+                {
+                    $arrKlantGegevens['idgemeente'] = $row->Idgemeente;
+                }
+                $this->db->where('klantID', $arrKlantGegevens['klantID']);
+                $this->db->update('klanten',$arrKlantGegevens);
+            }
+            
+            return '<br><br>
+                <div data-alert class="alert-box success radius">
+                        Uw klant werd bewerkt !
+                        <a href="#" class="close">&times;</a>
+                    </div>';
+    }
+    
+    public function KlantenFilterTonen($strStartWaarde, $strVeld){    
+        $this->db->select('*'); 
+        $this->db->from('klanten');
+        $this->db->like($strVeld, $strStartWaarde, 'after');
+        
+        $query = $this->db->get();
+        
+        if($query->num_rows() >0)
+        {
+            $strHTML = '<fieldset>
+                    <legend>Klanten</legend> 
+                    <table>
+                        <thead>
+                          <tr>
+                            <th>Gebruiker</th>
+                            <th width="20%">Bewerken</th>
+                          </tr>
+                        </thead>
+                        <tbody>';
+
+
+
+            foreach ($query->result() as $row){ 
+                $strHTML .= '<tr>
+                                <td>
+                                    '.$row->achternaam.'&nbsp'.$row->voornaam.'
+                                </td>
+                                <td align="center">
+                                    <form form method="POST">
+                                        <input type="hidden" name="klant_id" value="'.$row->klantID.'" >
+                                        <button type="submit" class="small button" name="Bewerken"><i class="step fi-pencil size-18"></i></button>
+                                    </form>
+                                </td>
+                            </tr>';     
+            } 
+            $strHTML .= '</tbody></table></fieldset>';
+        }
+        else
+        {
+            $strHTML = '<br><br>
+                <div data-alert class="alert-box alert radius">
+                        De klant die u zoekt bestaat nog niet.<br>
+                        U kan een klant toevoegen via het menu-item "Klanten"
+                        <a href="#" class="close">&times;</a>
+                    </div>';
+        }
+        
+        
+        return $strHTML;
+    }
+    
+    public function KlantTonen($intKlantID){
+        $this->db->select('*');
+        $this->db->where('klantID', $intKlantID);
+        $this->db->from('klanten');
+        $this->db->join('gemeente', 'klanten.idgemeente = gemeente.Idgemeente');
+
+        $query = $this->db->get();
+        foreach ($query->result() as $row)
+        {            
+            $strHTML = '<form method="post" name="frmGebruikerToevoegen" class="custom">
+            <fieldset>
+                <legend>Klant bewerken</legend>
+            <div class="row">
+                <div class="large-12 columns">
+                    <input type="hidden" value="'.$row->klantID.'" name="klantID"/>
+                    <label>KlantID : '.$row->klantID.'</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="large-12 columns">
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="large-6 columns">
+                    <label for="txtKlantAchternaam">Achternaam</label>
+                    <input type="text" value="'.$row->achternaam.'" name="txtKlantAchternaam" id="txtKlantAchternaam" required />
+                </div>
+                <div class="large-6 columns">
+                    <label for="txtKlantnaam">Voornaam</label>
+                    <input type="text" value="'.$row->voornaam.'" name="txtKlantVoornaam" id="txtKlantVoornaam" required />
+                </div>
+            </div>
+            <div class="row">
+                <div class="large-6 columns">
+                    <label for="txtStraat">Straat</label>
+                    <input type="text" value="'.$row->straat.'" name="txtStraat" id="txtStraat" required />
+                </div>
+                <div class="large-6 columns">
+                    <label for="txtHuisnummer">Huisnummer</label>
+                    <input type="text" value="'.$row->huisnummer.'" name="txtHuisnummer" id="txtHuisnummer" required />
+                </div>
+            </div>
+            <div class="row">
+                <div class="large-6 columns">
+                    <label for="txtGemeente">Gemeente</label>
+                    <input type="text" value="'.$row->gemeente.'" name="txtGemeente" id="txtGemeente" required />
+                </div>
+                <div class="large-6 columns">
+                    <label for="txtPostcode">Postcode</label>
+                    <input type="text" value="'.$row->postcode.'" name="txtPostcode" id="txtPostcode" required />
+                </div>
+            </div>
+            <div class="row">
+                <div class="large-6 columns">
+                    <label for="txtTelefoon">Telefoon</label>
+                    <input type="text" value="'.$row->telefoon.'" name="txtTelefoon" id="txtTelefoon" required />
+                </div>
+                <div class="large-6 columns">
+                    <label for="txtGsm">GSM</label>
+                    <input type="text" value="'.$row->gsm.'" name="txtGsm" id="txtGsm" required />
+                </div>
+            </div>
+            <div class="row">
+                <div class="large-12 columns">
+                    <label for="txtEmail">Email</label>
+                    <input type="text" value="'.$row->email.'" name="txtEmail" id="txtEmail" required />
+                </div>
+            </div>
+            <div class="row">
+                <div class="large-12 columns">
+                    <label for="txtOpmerking">Email</label>
+                    <textarea name="txtOpmerking" id="txtOpmerking" required>'.$row->opmerking.'</textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="large-12 columns">
+                    <hr>
+                </div>
+            </div>            
+            <div class="row">
+                <div class="large-6 columns">
+                    <button type="submit" class="small button" name="updateKlant">Opslaan</button>
+                </div>
+            </div>
+            </fieldset>
+            </form>';
+        }
+        return $strHTML;
+    }    
+    
+    public function SelectNaamForm()
+    {
+        $strHtml = '<form method="post" name="frmSelectGebruiker" class="custom">
+                        <fieldset>
+                         <div class="row">
+                            <div class="large-8 columns">
+                                <input type="text" placeholder="Klantgegeven" name="txtSelectNaam" id="txtSelectNaam"/>
+                            </div>
+                            <div class="large-4 columns" align="right">
+                                <button type="submit" class="small button" name="searchKlant"><i class="fi-magnifying-glass size-21"></i></button>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="large-8 columns">
+                                <select class="large" name="ddSelectItem">
+                                    <option value="achternaam">Achternaam</option>
+                                    <option value="voornaam">Voornaam</option>
+                                    <option value="straat">Straat</option>
+                                    <option value="telefoon">Telefoon</option>
+                                    <option value="gsm">Gsm</option>
+                                    <option value="email">E-mail</option>
+                                </select>
+                            </div>
+                        </div>
+                        </fieldset>
+                    </form>';
+        
+        return $strHtml;
+    }
+    
+    public function AlleKlantenTonen()
+    {
+        $this->db->select('*'); 
+        $this->db->from('klanten');
+        
+        $query = $this->db->get();
+        
+        $strHTML = '<table>
+                        <thead>
+                          <tr>
+                            <th>Klant ID</th>
+                            <th>Achternaam</th>
+                            <th>Voornaam</th>
+                            <th>Straat</th>
+                            <th>Nr°</th>
+                            <th>Gemeente</th>
+                            <th>Postcode</th>
+                            <th>Telefoon</th>
+                            <th>Gsm</th>
+                            <th>E-mail</th>                            
+                          </tr>
+                        </thead>
+                        <tbody>';
+        foreach ($query->result() as $row)
+        { 
+            $this->db->select('*'); 
+            $this->db->from('gemeente');
+            $this->db->where('Idgemeente', $row->idgemeente);
+            $queryGemeente = $this->db->get();
+            
+            
+            $strHTML .= '<tr>
+                            <td>
+                                '.$row->klantID.'
+                            </td>
+                            <td>
+                                '.$row->achternaam.'
+                            </td>
+                            <td>
+                                '.$row->voornaam.'
+                            </td>
+                            <td>
+                                '.$row->straat.'
+                            </td>
+                            <td>
+                                '.$row->huisnummer.'
+                            </td>';
+            
+                            foreach ($queryGemeente->result() as $rij)
+                            {
+                                $strHTML .='<td>
+                                                '.$rij->gemeente.'
+                                            </td>
+                                            <td>
+                                                '.$rij->postcode.'
+                                            </td>';
+                            }
+            
+            $strHTML .= '<td>
+                                '.$row->telefoon.'
+                            </td>
+                            <td>
+                                '.$row->gsm.'
+                            </td>
+                            <td>
+                                '.$row->email.'
+                            </td>
+                        </tr>';     
+        } 
+        $strHTML .= '</tbody></table>';
+        
+        return $strHTML;
     }
 }
 
