c2d5a4bad7d1b661fd764a71023abe3b7246ced0
Kevin Vissers <kevin.vissers@telenet.be>
2013-12-09 16:44:26 +0100

    materialen toevoegen aan afspraak
    functies aanroepen met
    "http://localhost/CodeIgniter_2.1.4/index.php/materialen/tonen/'afspraakid'"
    en
    "http://localhost/CodeIgniter_2.1.4/index.php/materialen/toevoegen/'afspraakid'"

---
+++ CodeIgniter_2.1.4/application/controllers/materialen.php
@@ -0,0 +1,101 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class materialen extends CI_Controller {
+    public function Tonen(){
+        if ( ! file_exists('application/views/pages/klantenBestand.php'))
+	{
+		// Whoops, we don't have a page for that!
+		show_404();
+	}
+        $this->load->library('Helper_Library');
+        $data = $this->helper_library->Init();
+        $blnPermission = $this->session->userdata('logged_in') ? true : false;
+        $session_data = $this->session->userdata('logged_in');
+        
+        $menuConfig = array(
+            'currentController' => 'gebruiker',
+            'loggedIn' => $blnPermission,
+            'user' => $session_data['username'],
+            'userRole' => $session_data['userrole']
+        );
+        
+        $this->load->library('Menu_Library', $menuConfig);
+        $this->load->model('Materiaal_model');
+        
+        if($blnPermission){
+            $data['menu'] = $this->menu_library->ToonMenu();
+            $data['title'] = 'Klant bewerken';
+            
+            $data['inhoud'] = $this->Materiaal_model->Tonen(1);
+                                                
+            $this->load->view('templates/header', $data);
+            $this->load->view('pages/klantenBestand', $data);
+            $data['device'] = $this->helper_library->CreateFooter();
+            $this->load->view('templates/footer', $data);
+        }
+        else
+        {
+            //redirect('login', 'refresh');
+            header('Location: '.site_url().'/login');
+        }
+    }
+    public function Toevoegen($intAfspraakId){
+        if ( ! file_exists('application/views/pages/klantenBestand.php'))
+	{
+		// Whoops, we don't have a page for that!
+		show_404();
+	}
+        $this->load->library('Helper_Library');
+        $data = $this->helper_library->Init();
+        $blnPermission = $this->session->userdata('logged_in') ? true : false;
+        $session_data = $this->session->userdata('logged_in');
+        
+        $menuConfig = array(
+            'currentController' => 'gebruiker',
+            'loggedIn' => $blnPermission,
+            'user' => $session_data['username'],
+            'userRole' => $session_data['userrole']
+        );
+        
+        $this->load->library('Menu_Library', $menuConfig);
+        $this->load->model('Materiaal_model');
+        
+        if($blnPermission){
+            $data['menu'] = $this->menu_library->ToonMenu();
+            $data['title'] = 'Klant bewerken';
+            $data['inhoud'] = $this->Materiaal_model->ToevoegFormulier($intAfspraakId);
+            
+            if(isset($_POST['materiaalToevoegen'])){
+                $arrMaterialen = array(
+                    "materiaalNaam" => $_POST['naam']
+                );
+                $arrMateriaallijst = array(
+                    "aantal" => $_POST['aantal'],
+                    "eenheid" => $_POST['eenheid']
+                );
+                $stat = $this->Materiaal_model->Toevoegen($arrMaterialen, $arrMateriaallijst, $intAfspraakId);
+                
+                if($stat){
+                    $data['inhoud'] = $this->Materiaal_model->Tonen($intAfspraakId);
+                }else{
+                    $data['inhoud'] = $stat;
+                }
+            }
+                                                
+            $this->load->view('templates/header', $data);
+            $this->load->view('pages/klantenBestand', $data);
+            $data['device'] = $this->helper_library->CreateFooter();
+            $this->load->view('templates/footer', $data);
+        }
+        else
+        {
+            //redirect('login', 'refresh');
+            header('Location: '.site_url().'/login');
+        }
+    }
+}
+?>
---
+++ CodeIgniter_2.1.4/application/models/materiaal_model.php
@@ -0,0 +1,132 @@
+<?php
+
+/**
+ * Materiaallijst_model
+ *
+ * @package             planningtool
+ * @author              Kevin Vissers <kevin.vissers@student.khlim.be>
+ * @version		1.0
+ * @date		01/11/2013
+ * @copyright (c)       2013, KHLIM-ict
+ *
+ * the Materiaallijst_model class has the following properties and methods:
+ * properties:  
+ * methods: 
+ *
+ */
+class Materiaal_model extends CI_Model {
+
+    function __construct(){
+        parent::__construct();
+    }
+    public function Tonen($intAfspraakID){
+        $objResult = $this->_Select($intAfspraakID);
+        $strHTML = '<div class="row">
+                        <div class="large-4 columns">
+                            Beschrijving
+                        </div>
+                        <div class="large-4 columns">
+                            Aantal
+                        </div>
+                        <div class="large-4 columns">
+                            Eenheid
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="large-12 columns">
+                            <hr>
+                        </div>
+                    </div>';
+        foreach ($objResult->result() as $row) {
+            $strHTML .= '<div class="row">';
+            $strHTML .= '<div class="large-4 columns">'.$row->materiaalNaam.'</div>
+                        <div class="large-4 columns">'.$row->aantal.'</div>
+                        <div class="large-4 columns">'.$row->eenheid.'</div>';
+            $strHTML .= '</div>';
+        }
+        return $strHTML;
+    }
+    public function ToevoegFormulier($intAfspraakID){
+        $strHTML = '<form method="post" name="frmKlantToevoegen" class="custom">
+            <fieldset>
+                <legend>Materiaal Toevoegen</legend>
+                
+            <div class="row">
+                <div class="large-12 columns">
+                    <label for="txtNaam">Naam</label>
+                    <input type="hidden" name="afspraakid" value="'.$intAfspraakID.'" >
+                    <input type="text" placeholder="Naam" id="txtNaam" name="naam" required >
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="large-12 columns">
+                    <hr>
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="large-8 columns">
+                    <label for="txtAantal">Aantal</label>
+                    <input type="text" placeholder="Aantal" id="txtAantal" name="aantal" required>
+                </div>
+                <div class="large-4 columns">
+                    <label for="txtEenheid">Eenheid</label>
+                    <input type="text" placeholder="Eenheid" id="txtEenheid" name="eenheid" required>
+                </div>
+            </div>           
+                 
+            <div class="row">
+                <div class="large-12 columns">
+                    <hr>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="large-6 columns">
+                    <input type="submit" class="small button" name="materiaalToevoegen" value="Toevoegen"/>
+                </div>
+            </div>
+            
+            </fieldset>
+            </form>';
+        return $strHTML;
+    }
+    public function Toevoegen($arrMaterialen,$arrMateriaallijst, $intAfspraakID){
+        return $this->_Insert($arrMaterialen, $arrMateriaallijst, $intAfspraakID);
+    }
+    public function Bewerken($arrGegevens, $intAfspraakID){
+        
+    }
+    public function Verwijderen($intMateriaalID, $intAfspraakID){
+        
+    }
+    private function _Select($intAfspraakID){
+        $this->db->select('*');
+        $this->db->from('materiaallijst');
+        $this->db->where('afspraakid', $intAfspraakID);
+        $this->db->join('materialen','materiaallijst.materiaalid = materialen.materiaalID');
+        
+        return $this->db->get();
+    }
+    private function _Insert($arrMaterialen,$arrMateriaallijst, $intAfspraakID){
+        try{
+            $this->db->insert('materialen', $arrMaterialen);
+            $intMateriaalID = $this->db->insert_id();
+            $arrMateriaallijst['afspraakid'] = $intAfspraakID;
+            $arrMateriaallijst['materiaalid'] = $intMateriaalID;
+            $this->db->insert('materiaallijst', $arrMateriaallijst);
+            return true;
+        }catch(PDOException $ex){
+            return $ex->getMessage();
+        }
+    }
+    private function _Update($arrMaterialen,$arrMateriaallijst, $intAfspraakID,$intIdMateriaalLijst){
+        
+    }
+    private function _Delete($intMateriaalID){
+        
+    }
+}
+
+?>
