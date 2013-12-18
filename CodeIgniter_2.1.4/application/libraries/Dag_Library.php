442f5b949531ebd19279569ede3e4a992d193547
DylanDerwael <1135195@student.khlim.be>
2013-12-18 12:04:37 +0100

    Update Dag_Library.php
    update dag library

--- CodeIgniter_2.1.4/application/libraries/Dag_Library.php
+++ CodeIgniter_2.1.4/application/libraries/Dag_Library.php
@@ -9,13 +9,8 @@ class Dag_Library {
         var $template       = '';
         var $day_type       = 'abr';
         var $show_next_prev = TRUE;
-        var $next_prev_url  = 'kalender/dagOverzicht/';
-/**
- * @access  public
- */    
-    public function __construct(){
-        // Do something with $params
-    }
+ 
+    public function __construct(){}
 
 
         function Calendar_dag($config = array()){
@@ -47,7 +42,7 @@ class Dag_Library {
         }
 
 
-        function GenerateView($dag,$maand,$jaar,$query){
+        function GenerateView($jaar,$maand,$dag,$query){
 
             if ($jaar == ''){ $jaar  = date("Y", $this->local_time);}
 
@@ -63,115 +58,182 @@ class Dag_Library {
 
             if(strlen($dag)==1){$dag='0'.$dag;}
 
-            $strHTML = "<div id='container'>
-                            <div id='tijdmenu'>
-                                <h1 style='margin-bottom:0;'>
-                                    <a href='{previous_url}'>&lt;&lt;</a>
-                                    ".$dag."/".$maand."/".$jaar."
-                                    <a href='{next_url}''>&gt;&gt;</a>
-                                </h1>
-                            </div>";
+            $adjusted_date = $this->adjust_date($dag,$maand, $jaar);
 
-            if ($query->num_rows() > 0){
-                foreach ($query->result_array() as $row){
-                  $strHTML.="<a href='#' class='detail'>
-                                <div class='tijd'>
-                                    ".$row['startTijd']."
-                                    ".$row['voornaam']." ".$row['achternaam']."
-                                </div>
-                                <div class='inhoud'>
-                                    ".$row['eindTijd']."
-                                    ".$row['straat']."
-                                </div>
-                            </a>";  
-                }
-            }
-            else{ $strHTML.= "Geen afspraken gevonden"; }
+            $dag    = $adjusted_date['dag'];
+            $maand  = $adjusted_date['maand'];
+            $jaar   = $adjusted_date['jaar'];
+
+            $this->parse_template();
+
+            $strHTML = $this->temp['main_open'];
             
-            $strHTML.="</div>";
 
+                $strHTML.= $this->temp['heading_row_start'];
+
+                    if ($this->show_next_prev == TRUE){
+
+                        $adjusted_date = $this->adjust_date($dag-1,$maand, $jaar);
+                        $strHTML.= str_replace('{previous_url}', site_url("/kalender/dagOverzicht/".$adjusted_date['jaar'].'/'.$adjusted_date['maand'].'/'.$adjusted_date['dag']), $this->temp['heading_prev_cell']);
+                    }
+
+                    $this->temp['heading_title_cell'] = str_replace('{datum}', $dag.'/'.$maand.'/'.$jaar, $this->temp['heading_title_cell']);
+                    $strHTML.= $this->temp['heading_title_cell'];
+                    
+                    if ($this->show_next_prev == TRUE){
+                        $adjusted_date = $this->adjust_date($dag+1,$maand, $jaar);
+                        $strHTML .= str_replace('{next_url}', site_url("/kalender/dagOverzicht/".$adjusted_date['jaar'].'/'.$adjusted_date['maand'].'/'.$adjusted_date['dag']), $this->temp['heading_next_cell']);
+                    }
+                $strHTML.= $this->temp['heading_row_end'];
+
+                if($query->num_rows() > 0){
+                    foreach ($query->result_array() as $row){
+                        $strHTML.= str_replace('{id}', $row['id'], $this->temp['date_start']);
+                    
+                        $strHTML.= str_replace('{content}', $row['startTijd']." ".$row['voornaam']." ".$row['achternaam'] , $this->temp['date_row_content']);
+                        $strHTML.= str_replace('{content}', $row['eindTijd']." ".$row['straat'], $this->temp['date_row_content']);
+
+                        $strHTML.= $this->temp['date_stop'];
+                    }
+                }
+                else{
+                    $strHTML.= "geen afspraken gevonden";
+                }
+                
+
+            $strHTML.= $this->temp['main_close'];
+            
+          
             return $strHTML;
          }
-         /*
-        if ($this->show_next_prev == TRUE)
-        {
-            // Add a trailing slash to the  URL if needed
-            $this->next_prev_url = preg_replace("/(.+?)\/*$/", "\\1/",  $this->next_prev_url);
 
-            $adjusted_date = $this->adjust_date($maand - 1, $jaar);
-            $out = str_replace('{previous_url}', $this->next_prev_url.$adjusted_date['jaar'].'/'.$adjusted_date['maand']);
+        function default_template(){
+            return  array (
+                            'main_open'                 => '<div>',
+                            'heading_row_start'         => '<div id="tijdmenu"><h1 style="margin-bottom:0;">',
+                            'heading_prev_cell'         => '<a href="{previous_url}">&lt;&lt;</a>',
+                            'heading_title_cell'        => '{datum}',
+                            'heading_next_cell'         => '<a href="{next_url}">&gt;&gt;</a>',
+                            'heading_row_end'           => '</h1></div>',
+                            'date_start'                => '<div class="inhoud"><a href="{afpraak}" "class="detail"> ',
+                            'date_row_content'          => '<div>{content}</div>',
+                            'date_stop'                 => '</a></div>',
+                            'main_close'                => '</div>',
+                        );
         }
-        */
-/*
-        // "next" maand link
-        if ($this->show_next_prev == TRUE)
-        {
-            $adjusted_date = $this->adjust_date($maand + 1, $jaar);
-            $out = str_replace('{next_url}', $this->next_prev_url.$adjusted_date['jaar'].'/'.$adjusted_date['maand']);
-        }
-*/
-        function adjust_date($maand, $jaar)
-        {
+        
+        //TODO kijk na php functie om dit simpeler te maken !!!
+        // adjust_date + dagenInMaand !!! 
+        // ??? date('d-m-Y', strtotime("+1 day")) ???
+        function adjust_date($dag,$maand, $jaar){
             $date = array();
 
+            $date['dag'] = $dag;
             $date['maand']  = $maand;
             $date['jaar']   = $jaar;
 
-            while ($date['maand'] > 12)
-            {
+            if($date['dag']> $this->dagenInMaand($maand,$jaar)){
+                $date['dag'] -= $this->dagenInMaand($maand,$jaar);
+                $date['maand']++;
+            }
+            if($date['dag']<=0){
+                if($date['maand']== 1){
+                    $date['dag'] += 31;
+                }
+                else{
+                    $date['dag'] += $this->dagenInMaand($maand -1,$jaar);
+                }
+                $date['maand']--;
+            }
+            while ($date['maand'] > 12){
                 $date['maand'] -= 12;
                 $date['jaar']++;
             }
-
-            while ($date['maand'] <= 0)
-            {
+            while ($date['maand'] <= 0){
                 $date['maand'] += 12;
                 $date['jaar']--;
             }
-
-            if (strlen($date['maand']) == 1)
-            {
+            if (strlen($date['maand']) == 1){
                 $date['maand'] = '0'.$date['maand'];
             }
-
+            if (strlen($date['dag']) == 1){
+                $date['dag'] = '0'.$date['dag'];
+            }
             return $date;
         }
-    
+    function dagenInMaand($maand, $jaar)
+    {
+        $dagen_in_maand  = array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+
+        if ($maand < 1 OR $maand > 12){
+            return 0;
+        }
+        if ($maand == 2){
+            if ($jaar % 400 == 0 OR ($jaar % 4 == 0 AND $jaar % 100 != 0)){
+                return 29;
+            }
+        }
+
+        return $dagen_in_maand[$maand - 1];
     }
+        
+     function parse_template(){
+        $this->temp = $this->default_template();
+        
+        if ($this->template == '')
+        {
+            return;
+        }
+
+        $today = array('cal_cell_start_today', 'cal_cell_content_today', 'cal_cell_no_content_today', 'cal_cell_end_today');
+
+        foreach (array('main_open', 'main_close', 'heading_row_start', 'heading_prev_cell', 'heading_title_cell', 'heading_next_cell', 'heading_row_end', 'date_start', 'week_day_cell', 'date_end', 'cal_row_start', 'cal_cell_start', 'cal_cell_content', 'cal_cell_no_content',  'cal_cell_blank', 'cal_cell_end', 'cal_row_end', 'cal_cell_start_today', 'cal_cell_content_today', 'cal_cell_no_content_today', 'cal_cell_end_today') as $val)
+        {
+            if (preg_match("/\{".$val."\}(.*?)\{\/".$val."\}/si", $this->template, $match))
+            {
+                $this->temp[$val] = $match['1'];
+            }
+            else
+            {
+                if (in_array($val, $today, TRUE))
+                {
+                    $this->temp[$val] = $this->temp[str_replace('_today', '', $val)];
+                }
+            }
+        }
+    }
+}
     
-    /* TODO: CSS toevoegen
+/* HTML output + css id's en tags 
 
-            #container{
-                width:25em;
-                text-align: center;
-            }
-            .inhoud{
-                background-color:#EEEEEE;
-                height:3em;
-                width:20em;
-                float:left;
-            }
-            .tijd{
-                background-color:#444499;
-                height:3em;
-                width:5em;
-                float:left;
-            }
-            #tijdmenu{
-                background-color:#5555AA;
-                
-            }
-            .detail{
-                a:link {color:#000000;}      
-                a:visited {color:#FFFFFF;}  
-                a:hover {color:#FFFFFF;}  
-                a:active {color:#FFFFFF;}  
-            }
+    id container =  omkaderd volledig
+    id tijdmenu =  omkadering datum navigatie
+    class inhoud =  omkadering van een afspraak
+    class detail =  layout van de a tag te verbeteren
 
-    */
+            <div id='container'>
+                    <div id='tijdmenu'>
+                        <h1 style='margin-bottom:0;'>
+                            <a href='link vorige dag'>&lt;&lt;</a>
+                                → datum
+                            <a href='link volgende dag''>&gt;&gt;</a>
+                        </h1>
+                    </div>
+                    <div class="inhoud">
+                        <a href='link naar de afspraak' class='detail'>
+                                <div >
+                                    → startTijd Voornaam Achternaam
+                                </div>
+                                <div>
+                                    → eindTijd Adress
+                                </div>
+                        </a>
+                    </div>
+            </div>
+ */
      
-    // END Calendar_dag class
+    // END Dag_Library class
      
-    /* End of file Calendar_dag.php */
-    /* Location: ./system/application/libraries/Calendar_dag.php */
+    /* End of file Dag_Library.php */
+    /* Location: ./application/libraries/Dag_Library.php */
 
