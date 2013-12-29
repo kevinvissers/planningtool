8adec97abc0df1d47b8d5f32290fa2d8f015ab14
Kevin Vissers <kevin.vissers@telenet.be>
2013-12-29 19:08:23 +0100

    opmaak dagoverzicht (view + css)

--- CodeIgniter_2.1.4/application/views/pages/dagOverzicht.php
+++ CodeIgniter_2.1.4/application/views/pages/dagOverzicht.php
@@ -1,11 +1,18 @@
 <!-- Main Page Content and Sidebar -->
 <div class="row">
-
-<!-- Main Blog Content -->
-<div class="large-9 columns" role="content">
-    <h3>dag overzicht</h3>
-    <div class="kalender_dag_inhoud" align="center"><?php echo $kalender; ?></div>
-</div>
-<!-- End Main Content -->
+    <!-- Main Blog Content -->
+    <div class="large-6 columns" role="content">
+        <!--<h3>dag overzicht</h3>-->
+        <?php echo $kalender; ?>
+    </div>
+    <aside class="large-6 columns">
+      <div class="panel">
+        <ul class="side-nav">
+            <?php echo $eigenschappen ?>
+        </ul>
+        <!--<a href="#">Read More &rarr;</a>-->
+      </div>
+    </aside>
+    <!-- End Main Content -->
 </div>
 <!-- End Main Content and Sidebar -->
